5de400233a70f3f7adff8870b6616c95c137cf62
Mehul Singhal <mehulsinghal144@gmail.com>
2021-12-15 19:33:44 +0530

    Added Gotty cut and bonus chances.

--- app/src/main/java/com/example/ludo/MainActivity.java
+++ app/src/main/java/com/example/ludo/MainActivity.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
-
+//string facing
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         put('b', 3);
     }};
     private int curr_player = 0;
+    //constants in capital
     private final String blocks = "rgyb";
     private int curr_die_val = 0;
     private boolean turn_pending = false;
@@ -49,88 +50,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-//        binding.diceR.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(curr_player==1)
-//                {
-//                    curr_player++;
-//                    generate_and_disp("diceR");
-//                }
-//                else
-//                {
-//                    disp_toast(curr_player);
-//                }
-//            }
-//        });
-//
-//        binding.diceG.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(curr_player==2)
-//                {
-//                    curr_player++;
-//                    generate_and_disp("diceG");
-//                }
-//                else
-//                {
-//                    disp_toast(curr_player);
-//                }
-//            }
-//        });
-//
-//        binding.diceY.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(curr_player==3)
-//                {
-//                    curr_player++;
-//                    generate_and_disp("diceY");
-//                }
-//                else
-//                {
-//                    disp_toast(curr_player);
-//                }
-//            }
-//        });
-//
-//        binding.diceB.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(curr_player==4)
-//                {
-//                    curr_player=1;
-//                    generate_and_disp("diceB");
-//                }
-//                else
-//                {
-//                    disp_toast(curr_player);
-//                }
-//            }
-//        });
         StartGame();
     }
 
     private void disp_toast(int turn) {
         switch (turn)
         {
-            case 1:Toast.makeText(MainActivity.this, "Red's turn", Toast.LENGTH_SHORT).show();
+            case 1:Toast.makeText(MainActivity.this,
+                    "Red's turn", Toast.LENGTH_SHORT).show();
             break;
-            case 2:Toast.makeText(MainActivity.this, "Green's turn", Toast.LENGTH_SHORT).show();
+            case 2:Toast.makeText(MainActivity.this,
+                    "Green's turn", Toast.LENGTH_SHORT).show();
             break;
-            case 3:Toast.makeText(MainActivity.this, "Yellow's turn", Toast.LENGTH_SHORT).show();
+            case 3:Toast.makeText(MainActivity.this,
+                    "Yellow's turn", Toast.LENGTH_SHORT).show();
             break;
-            default:Toast.makeText(MainActivity.this, "Blue's turn", Toast.LENGTH_SHORT).show();
+            default:Toast.makeText(MainActivity.this,
+                    "Blue's turn", Toast.LENGTH_SHORT).show();
         }
     }
-
+    //generateAndDisp
     private void generate_and_disp(String dice) {
         Random rand = new Random();
         int x = rand.nextInt(6)+1;
         curr_die_val = x;
         int id = this.getResources().getIdentifier(dice,"id",this.getPackageName());
-        int im_id = this.getResources().getIdentifier(die_vals_to_image_map.get(x),"drawable",this.getPackageName());
+        int im_id = this.getResources().getIdentifier(die_vals_to_image_map.get(x),
+                                                        "drawable",this.getPackageName());
         Animation aniRotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
         ImageView img = (ImageView) findViewById(id);
         img.startAnimation(aniRotate);
@@ -141,12 +87,12 @@ public class MainActivity extends AppCompatActivity {
         if(!turn_pending)
             return;
         String cell_id = v.getResources().getResourceEntryName(v.getId());
-        int gotty_idx = players.get(curr_player).gotty_at_cell(cell_id);
+        ArrayList<Integer> gotty_idx = players.get(curr_player).gotties_at_cell(cell_id);
 //        System.out.println("Src: "+ cell_id+" "+curr_player+" "+gotty_idx);
         //Click on cell on which there is no gotty of particular color.
-        if(gotty_idx == -1)
+        if(gotty_idx.size() == 0)
             return;
-        gotty curr_event = players.get(curr_player).gotties.get(gotty_idx);
+        gotty curr_event = players.get(curr_player).gotties.get(gotty_idx.get(0));
         int curr_cell = Integer.parseInt(cell_id.substring(1));
         int next_cell = curr_cell+curr_die_val;
 //        System.out.println(curr_cell+" "+next_cell);
@@ -170,26 +116,55 @@ public class MainActivity extends AppCompatActivity {
         }
         //When gotty remains in same block
         else{
+            //DO nothing extra
         }
         String dest_id = blocks.charAt(curr_event.block)
                                 +Integer.toString(next_cell);
-        place_gotty_at_location(gotty_idx, dest_id);
+        if(next_cell != 9 && next_cell != 4) {
+            for (int i = 0; i < 4; i++) {
+                if (i == curr_player)
+                    continue;
+                ArrayList<Integer> defeated_gotties_index = new ArrayList<Integer>(0);
+                for (int j = 0; j < 4; j++) {
+                    ArrayList<Integer> temp = players.get(i).gotties_at_cell(dest_id);
+                    if (temp.size() != 0)
+                        defeated_gotties_index = temp;
+                }
+                if(defeated_gotties_index.size() == 1 || defeated_gotties_index.size() == 3){
+                    ArrayList<String> empty_places = new ArrayList<String>(0);
+                    for(int j = 0; j < 4; j++){
+                        String loc = blocks.charAt(i)+"p"+Integer.toString(j+1);
+                        if(players.get(i).gotties_at_cell(loc).size() == 0)
+                            empty_places.add(loc);
+                    }
+                    int j = 0;
+                    for(int k: defeated_gotties_index){
+                        place_gotty_at_location(i, k, empty_places.get(j));
+                        players.get(i).gotties.set(k,new gotty(empty_places.get(j),i));
+                        j++;
+                        players.get(curr_player).open_gotties_count--;
+                    }
+                    break;
+                }
+            }
+        }
+        place_gotty_at_location(curr_player, gotty_idx.get(0), dest_id);
         curr_event.pos = dest_id;
-        players.get(curr_player).gotties.set(gotty_idx,curr_event);
-        curr_player = (curr_player+1)%4;
+        players.get(curr_player).gotties.set(gotty_idx.get(0),curr_event);
+        if(curr_die_val != 6)
+            curr_player = (curr_player+1)%4;
         turn_pending = false;
     }
 
-    public void place_gotty_at_location(int gotty_idx, String dest_id){
+    public void place_gotty_at_location(int player_idx, int gotty_idx, String dest_id){
 //        System.out.println(dest_id);
-        String curr = players.get(curr_player).gotties.get(gotty_idx).pos;
+        String curr = players.get(player_idx).gotties.get(gotty_idx).pos;
         int id1 = this.getResources().getIdentifier(curr,"id",this.getPackageName());
         int id2 = this.getResources().getIdentifier(dest_id,"id",this.getPackageName());
         ImageView img1 = (ImageView) findViewById(id1);
         ImageView img2 = (ImageView) findViewById(id2);
         img1.setImageResource(0);
-        switch(curr_player)
-        {
+        switch(player_idx) {
             case 0:img2.setImageResource(R.drawable.red_pawn);
                 break;
             case 1:img2.setImageResource(R.drawable.green_pawn);
@@ -198,22 +173,26 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:img2.setImageResource(R.drawable.blue_pawn);
         }
-
 //        System.out.println("dest: "+dest_id);
     }
     public void onClick_start(View v){
         if(!turn_pending)
             return;
+        if(curr_die_val != 6){
+            turn_pending = false;
+            curr_player = (curr_player+1)%4;
+            return;
+        }
         String cell_id = v.getResources().getResourceEntryName(v.getId());
-        int gotty_idx = players.get(curr_player).gotty_at_cell(cell_id);
+        ArrayList<Integer> gotty_idx = players.get(curr_player).gotties_at_cell(cell_id);
 //        System.out.println(gotty_idx+" "+gotty_idx);
-        if(gotty_idx == -1)
+        if(gotty_idx.size() == 0)
             return;
         String dest_id = blocks.charAt(curr_player)+"9";
         turn_pending = false;
-        place_gotty_at_location(gotty_idx, dest_id);
-        players.get(curr_player).gotties.get(gotty_idx).pos = dest_id;
-        curr_player = (curr_player+1)%4;
+        place_gotty_at_location(curr_player, gotty_idx.get(0), dest_id);
+        players.get(curr_player).gotties.get(gotty_idx.get(0)).pos = dest_id;
+        players.get(curr_player).open_gotties_count++;
     }
 
     public void onClick_dice(View v){
@@ -224,6 +203,10 @@ public class MainActivity extends AppCompatActivity {
         if(die_id.equals(temp)){
             turn_pending = true;
             generate_and_disp(turn_to_die_map.get(curr_player));
+            if(curr_die_val != 6 && players.get(curr_player).open_gotties_count == 0){
+                curr_player = (curr_player+1)%4;
+                turn_pending = false;
+            }
         }
         else
             disp_toast((curr_player+1)%4);
