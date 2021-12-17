354336cc0da9ec3a313bd9c4c16eb720985b7650
naman-singhal-025 <namansinghal025@gmail.com>
2021-12-17 21:34:44 +0530

    login and signup activities

---
+++ app/src/main/java/com/example/ludo/SignIn.java
@@ -0,0 +1,91 @@
+package com.example.ludo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.ludo.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignIn extends AppCompatActivity {
+
+    ActivitySignInBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(SignIn.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Logging in");
+
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(binding.etEmail.getText().toString().isEmpty())
+                {
+                    binding.etEmail.setError("Enter your E-mail id");
+                    return;
+                }
+                if(binding.etPassword.getText().toString().isEmpty())
+                {
+                    binding.etPassword.setError("Required Password");
+                    return;
+                }
+                progressDialog.show();
+                auth.signInWithEmailAndPassword(binding.etEmail.getText().toString(),
+                        binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressDialog.dismiss();
+                                if(task.isSuccessful())
+                                {
+
+                                    Intent intent = new Intent(SignIn.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                                else
+                                {
+                                    Toast.makeText(SignIn.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        binding.tvClickSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        if(auth.getCurrentUser()!=null)
+        {
+            Intent intent = new Intent(SignIn.this,MainActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
